fe09e9765c82a1cc465a6a47fb3c198c44afadcd
Fabio Spampinato <spampinabio@gmail.com>
2024-01-30 17:51:56 +0000

    Ensuring the result of a deserialized function is sent to the right realm

--- src/types/function.ts
+++ src/types/function.ts
@@ -33,12 +33,12 @@ class _Function extends Type<Function> {
 
   call ( id: string, ...args: unknown[] ) : void {
 
-    const [realm, fnId, resultId] = id.split ( '-' );
-    const fn = this.siero.contexts.id2function[fnId];
+    const [targetRealm, sourceRealm, fnId, resultId] = id.split ( '-' );
+    const fn = this.siero.contexts.id2function[`${sourceRealm}-${fnId}`];
 
     if ( !fn ) {
 
-      this.siero.realms.call ( realm, 'function.settle', [id, false, new ReferenceError ( 'Function not found' )] );
+      this.siero.realms.call ( targetRealm, 'function.settle', [id, false, new ReferenceError ( 'Function not found' )] );
 
     } else {
 
@@ -48,20 +48,20 @@ class _Function extends Type<Function> {
 
         if ( result instanceof Promise ) {
 
-          const onResolve = ( value: unknown ) => this.siero.realms.call ( realm, 'function.settle', [id, true, value] );
-          const onReject = ( error: unknown ) => this.siero.realms.call ( realm, 'function.settle', [id, false, error] );
+          const onResolve = ( value: unknown ) => this.siero.realms.call ( targetRealm, 'function.settle', [id, true, value] );
+          const onReject = ( error: unknown ) => this.siero.realms.call ( targetRealm, 'function.settle', [id, false, error] );
 
           result.then ( onResolve, onReject );
 
         } else {
 
-          this.siero.realms.call ( realm, 'function.settle', [id, true, result] );
+          this.siero.realms.call ( targetRealm, 'function.settle', [id, true, result] );
 
         }
 
       } catch ( error: unknown ) {
 
-        this.siero.realms.call ( realm, 'function.settle', [id, false, error] );
+        this.siero.realms.call ( targetRealm, 'function.settle', [id, false, error] );
 
       }
 
@@ -100,14 +100,19 @@ class _Function extends Type<Function> {
 
   deserialize ( value: string, options?: DeserializeOptions ): Function {
 
-    const [source, fnId] = value.split ( '-' );
-
     const fn = ( this.siero.contexts.id2function[value] ||= ( ...args: unknown[] ) => {
-      const id = `${value}-${this.siero.contexts.functionResultCounter++}`;
+
+      const targetRealm = this.siero.realm;
+      const [sourceRealm, fnId] = value.split ( '-' );
+      const resultId = this.siero.contexts.functionResultCounter++;
+      const id = `${targetRealm}-${sourceRealm}-${fnId}-${resultId}`;
       const promise = promiseWithResolvers ();
+
       this.siero.contexts.id2functionResult[id] = promise;
-      this.siero.realms.call ( source, 'function.call', [id, ...args] );
+      this.siero.realms.call ( sourceRealm, 'function.call', [id, ...args] );
+
       return promise.promise;
+
     });
 
     const id = mapGetOrSet ( this.siero.contexts.function2id, fn, () => value );
